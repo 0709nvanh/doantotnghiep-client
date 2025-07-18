becf503533dd243269039380d5c9057d3c9b6801
truongmanhdung <truongmanhdung04@gmail.com>
2025-07-18 10:16:06 +0700

    upcode

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,14 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/doantotnghiep-client",
-  plugins: [react()],
-  server: {
-    cors: true,
-    port: 3000,
-  },
-  preview: {
-    port: 3000,
-    cors: true,
-  },
-  resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: path.resolve(__dirname, "src"),
-      },
-    ],
-  },
-  build: {
-    outDir: "build",
-    assetsDir: "static",
-  },
+	base: "/doantotnghiep-client",
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: "@",
+				replacement: path.resolve(__dirname, "src"),
+			},
+		],
+	},
 });
